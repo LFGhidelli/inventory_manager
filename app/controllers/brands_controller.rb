ee5952305b24bf88a968e2a76b9db132cc031bb3
Luis Ghidelli <luisfernando.ghidelli@gmail.com>
2025-09-18 18:06:24 +0100

    controllers, views, forms and search bar

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,7 +1,55 @@
 class BrandsController < ApplicationController
   def index
+    if params[:query].present?
+      @brands = Brand.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @brands = Brand.all.order(:name)
+    end
   end
 
   def show
+  end
+
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.new(name: params[:name], description: params[:description])
+    @brand.save!
+
+    if @brand.persisted?
+      flash[:notice] = "Marca criada com sucesso"
+      redirect_to brands_path
+    else
+      flash[:alert] = "Algo deu errado"
+      redirect_to brands_path
+    end
+  end
+
+  def edit
+    @brand = Brand.find(params[:id])
+  end
+
+  def update
+    @brand = Brand.find(params[:id])
+  
+    if @brand.update(brand_params)
+      redirect_to brands_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @brand = Brand.find(params[:id])
+
+    @brand.destroy
+    redirect_to brands_path
+  end
+
+  private
+
+  def brand_params
+    params.expect(brand: [ :name, :description ])
   end
 end
